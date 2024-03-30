4ea316c55553de2e3c1965beb4b4c3b086d9a60c
Varun Gandhi <varungandhi@vl965-172-31-9-128.wireless.umass.edu>
2024-03-30 10:42:19 -0400

    add types for globals vars and x-functions-key and its token val to header

--- src/globals.ts
+++ src/globals.ts
@@ -3,13 +3,14 @@
  * We've put a few here that we know you will need.
  * Fill in the blank for each one
  */
-export const MY_BU_ID = "";
-export const BASE_API_URL = "https://spark-se-assessment-api.azurewebsites.net/api";
+export const MY_BU_ID: number = 32832379;  //converted to ts code
+export const BASE_API_URL: string = "https://spark-se-assessment-api.azurewebsites.net/api"; //converted to ts code
 // You can get this from Gradescope aka x-functions-key
-export const TOKEN = "";
+export const TOKEN: string = "6se7z2q8WGtkxBlXp_YpU-oPq53Av-y_GSYiKyS_COn6AzFuTjj4BQ=="; //converted to ts code
 // This is a helper function to generate the headers with the x-functions-key attached
-export const GET_DEFAULT_HEADERS = () => {
+export const GET_DEFAULT_HEADERS = (): Headers => {  //converted to ts code
   var headers = new Headers();
+  headers.append('x-functions-key', TOKEN);
   // You will need to add another header here
   // If you do not, the API will reject your request (:
   return headers;
